e852b11314f1d38686a4a30cfe45fd2887d587ce
J. Alberto González <45895462+JAlbertoGonzalez@users.noreply.github.com>
2020-01-14 21:00:29 +0000

    contacts info

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const querystring = require('querystring')
 const url = require('url')
+const axios = require('axios')
 
 const router = express.Router()
 
 const Model = require('../services/modelService')
+
+function getUrlParams(req) {
+    const rawUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+    let parsedUrl = url.parse(rawUrl);
+    let parsedQs = querystring.parse(parsedUrl.query);
+    return parsedQs
+}
 
 Model.then(db => {
     const Models = db.models
@@ -35,9 +43,7 @@ Model.then(db => {
     })
 
     router.get('/contacts/online', (req, res) => {
-        const rawUrl = req.protocol + '://' + req.get('host') + req.originalUrl
-        let parsedUrl = url.parse(rawUrl);
-        let parsedQs = querystring.parse(parsedUrl.query);
+        const parsedQs = getUrlParams(req)
 
         Models.Contact.find({ ...parsedQs, timeoutRate: { $lt: 0.04 } }, (err, result) => {
             if (err) {
@@ -49,9 +55,7 @@ Model.then(db => {
     })
 
     router.get('/users', (req, res) => {
-        const rawUrl = req.protocol + '://' + req.get('host') + req.originalUrl
-        let parsedUrl = url.parse(rawUrl);
-        let parsedQs = querystring.parse(parsedUrl.query);
+        const parsedQs = getUrlParams(req)
 
         Models.User.find(parsedQs, (err, result) => {
             if (err) {
@@ -72,8 +76,108 @@ Model.then(db => {
                 res.status(!result ? 400 : 200).send(result)
             }
         })
-
     })
+
+    router.get('/user/:email/usage', (req, res) => {
+        const email = req.params.email
+
+        Models.Bucket.aggregate([
+            {
+                $match: {
+                    user: email
+                }
+            },
+            {
+                $lookup: {
+                    from: 'bucketentries',
+                    localField: '_id',
+                    foreignField: 'bucket',
+                    as: 'join1'
+                }
+            },
+            {
+                $unwind: {
+                    path: '$join1'
+                }
+            },
+            {
+                $lookup: {
+                    from: "frames",
+                    localField: "join1.frame",
+                    foreignField: "_id",
+                    as: "join2"
+                }
+            },
+            {
+                $unwind: {
+                    path: '$join2'
+                }
+            },
+            {
+                $project: {
+                    _id: '$join2._id',
+                    user: '$join2.user',
+                    size: '$join2.size'
+                }
+            },
+            {
+                $group: {
+                    _id: '$user',
+                    total: { $sum: '$size' }
+                }
+            }
+        ], (err, results) => {
+            if (err || !results || results.length == 0) {
+                res.status(500).send({ error: 'Check server log for details' });
+            }
+            else {
+                res.status(200).send(results[0])
+            }
+        })
+    })
+
+    router.get('/payments/contacts', (req, res) => {
+        const parsedQs = getUrlParams(req)
+        const days = parsedQs.days || 2
+
+        var currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() - days)
+
+        Models.Contact.aggregate([
+            {
+                $match: {
+                    timeoutRate: { $lt: 0.04 },
+                    lastSeen: { $gte: currentDate }
+                }
+            },
+            {
+                $lookup: {
+                    from: 'shards',
+                    localField: '_id',
+                    foreignField: 'contracts.nodeID',
+                    as: 'shards'
+                }
+            },
+            {
+                $unwind: {
+                    'path': '$shards.contracts'
+                }
+            },
+            {
+                $skip: 1
+            },
+            {
+                $limit: 1
+            }
+        ], (err, results) => {
+            if (err) {
+                res.status(500).send({ error: err })
+            } else {
+                res.status(200).send(results)
+            }
+        })
+    })
+
 })
 
 
